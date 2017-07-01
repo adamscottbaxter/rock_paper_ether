f7c24f661ef01c5fd2b7d5d65070f8f878808d24
adamscottbaxter <adamscottbaxter@gmail.com>
2017-06-30 23:32:56 -0500

    Add View Hidden button

--- application.js
+++ application.js
@@ -3,4 +3,11 @@
     currentStage();
     pollStage();
   }, 5000);
+   
+  showHidden = function(){
+    var allDivs = document.querySelectorAll("[data-stage-display]")
+    for (var i = allDivs.length - 1; i >= 0; i--) {
+      allDivs[i].classList.remove('hidden');
+    }
+  }
 })();
